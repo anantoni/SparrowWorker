29f808f96abc25f77aab07c777c6f88681425107
tkaramp <thomas.karamp@gmail.com>
2014-07-07 21:00:13 +0300

    SparrowWorker: 	* split into classes 	* thread pool added 	* new thread class for task execution 	* handler written 	* UNSTABLE not tested yet

--- src/httpworker/HttpWorker.java
+++ src/httpworker/HttpWorker.java
@@ -6,51 +6,26 @@
 
 package httpworker;
 
-import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.URL;
 import java.security.KeyStore;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocketFactory;
-import org.apache.http.ConnectionClosedException;
-import org.apache.http.HttpConnectionFactory;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpEntityEnclosingRequest;
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpServerConnection;
-import org.apache.http.HttpStatus;
-import org.apache.http.MethodNotSupportedException;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.examples.ElementalHttpServer;
-import org.apache.http.impl.DefaultBHttpServerConnection;
-import org.apache.http.impl.DefaultBHttpServerConnectionFactory;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpProcessor;
 import org.apache.http.protocol.HttpProcessorBuilder;
-import org.apache.http.protocol.HttpRequestHandler;
 import org.apache.http.protocol.HttpService;
 import org.apache.http.protocol.ResponseConnControl;
 import org.apache.http.protocol.ResponseContent;
 import org.apache.http.protocol.ResponseDate;
 import org.apache.http.protocol.ResponseServer;
 import org.apache.http.protocol.UriHttpRequestHandlerMapper;
-import org.apache.http.util.EntityUtils;
+
 
 /**
  *
@@ -63,16 +38,10 @@ public class HttpWorker {
      * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception {
-        //Creating shared object to store requested tasks
-        BlockingQueue taskQueue = new LinkedBlockingQueue();
-        
-        ExecutorService threadPool = Executors.newFixedThreadPool(3);
-        
-        //threadPool.execute(new Consumer("1", requestQueue));
-        //threadPool.execute(new Consumer("2", requestQueue);
-        //threadPool.execute(new Consumer("3", requestQueue));
-        //threadPool.execute(new Consumer("4", requestQueue);
-        
+        int fixedExecutorSize = 4;
+        //Creating fixed size executor
+        ThreadPoolExecutor taskExecutor = new ThreadPoolExecutor(fixedExecutorSize, fixedExecutorSize,0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+
         // TODO code application logic here
         int port = 8080;
 
@@ -85,7 +54,7 @@ public class HttpWorker {
 
         // Set up request handlers
         UriHttpRequestHandlerMapper reqistry = new UriHttpRequestHandlerMapper();
-        reqistry.register("*", new RequestHandler(taskQueue));
+        reqistry.register("*", new RequestHandler(taskExecutor));
 
         // Set up the HTTP service
         HttpService httpService = new HttpService(httpproc, reqistry);
@@ -114,186 +83,12 @@ public class HttpWorker {
         System.out.println("Thread created");
         t.setDaemon(false);
         t.start();
-    }
-
-    static class RequestHandler implements HttpRequestHandler  {
-        private final BlockingQueue taskQueue;         
-        
-        // Pass reference to the requestsQueue to the RequestHandler
-        public RequestHandler(BlockingQueue taskQueue) {
-            super();
-            this.taskQueue = taskQueue;     
+        t.join();
+        taskExecutor.shutdown();
+        while (!taskExecutor.isTerminated()) {
         }
-
-        @Override
-        public void handle(
-                final HttpRequest request,
-                final HttpResponse response,
-                final HttpContext context) throws HttpException, IOException {
-
-            String method = request.getRequestLine().getMethod().toUpperCase(Locale.ENGLISH);
-            if (!method.equals("GET") && !method.equals("HEAD") && !method.equals("POST")) {
-                throw new MethodNotSupportedException(method + " method not supported");
-            }
-
-            if (request instanceof HttpEntityEnclosingRequest) {
-                HttpEntity entity = ((HttpEntityEnclosingRequest) request).getEntity();
-                //byte[] entityContent = EntityUtils.toByteArray(entity);
-                String entityString = EntityUtils.toString(entity);
-                System.out.println("Incoming entity content (string): " + entityString);
-                //System.out.println("Incoming entity content (bytes): " + entityContent.length);
-                //String a = new String(entityContent);
-               // System.out.println(a);
-                
-                // Parse HTTP request
-                Map<String,String> requestArgs = parseHttpSchedulerRequest(entityString);
-                
-                // If task requst
-                if (requestArgs.size() == 2) {
-                    assert requestArgs.containsKey("job-id") && requestArgs.containsKey("task-command");
-                    System.out.println("Adding task command to queue");
-                    String taskToProcess = requestArgs.get("task-command");
-                    try {
-                        taskQueue.put(taskToProcess);     // Add task to the request queue
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(HttpWorker.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }      
-                // If probe request
-                else if (requestArgs.size() == 1) {
-                    assert requestArgs.containsKey("probe");
-                }
-            }
-                    
-            // TODO: wait for result of task processing before returning the result to the scheduler
-            response.setStatusCode(HttpStatus.SC_OK);
-            StringEntity entity = new StringEntity("result:success");
-            response.setEntity(entity);
-        }
-        
-        // Parse HTTP requests with the following form: 
-        // job-id=1&task-command=sleep+240s
-        Map<String, String> parseHttpSchedulerRequest(String httpRequest) {
-            String[] requestArguments = httpRequest.split("&");
-            Map<String, String> argsMap = new HashMap<>();
-
-            if (requestArguments.length != 2 && requestArguments.length != 1 ) {
-                System.err.println("Invalid HTTP request: " + httpRequest);
-                return null; 
-            }
-            else if (requestArguments.length == 2) {
-                int counter = 0;
-                for ( String arg : requestArguments ) {
-                    String[] keyValuePair = arg.split("=");
-                    if (counter == 0) {
-                        if ( keyValuePair[0].equals("job-id") ) {
-                            assert keyValuePair[1].matches("[0-9]+");
-                            argsMap.put( keyValuePair[0], keyValuePair[1]);
-                        }
-                        else {
-                            System.err.println("Invalid argument - expecting job-id");
-                        }
-                    }
-                    else if (counter == 1) {
-                        if ( keyValuePair[0].equals("task-command") ) {
-                            argsMap.put( keyValuePair[0], keyValuePair[1]);
-                        }
-                        else {
-                            System.err.println("Invalid argument - expecting task-command");
-                        }
-                    }
-                    counter++;
-                }
-            }
-            else if (requestArguments.length == 1) {
-                String[] keyValuePair = requestArguments[0].split("=");
-                if ( keyValuePair[0].equals("probe") ) 
-                    argsMap.put( keyValuePair[0], keyValuePair[1]);
-                else 
-                    System.err.println("Invalid argument - expecting probe request");
-            }
-            return argsMap;
-        }
-    }
-
-    static class RequestListenerThread extends Thread {
-
-        private final HttpConnectionFactory<DefaultBHttpServerConnection> connFactory;
-        private final ServerSocket serversocket;
-        private final HttpService httpService;
-
-        public RequestListenerThread(
-                final int port,
-                final HttpService httpService,
-                final SSLServerSocketFactory sf) throws IOException {
-            this.connFactory = DefaultBHttpServerConnectionFactory.INSTANCE;
-            this.serversocket = sf != null ? sf.createServerSocket(port) : new ServerSocket(port);
-            this.httpService = httpService;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("Listening on port " + this.serversocket.getLocalPort());
-            while (!Thread.interrupted()) {
-                try {
-                    System.out.println("print #1");
-                    // Set up HTTP connection
-                    Socket socket = this.serversocket.accept();
-                    System.out.println("Incoming connection from " + socket.getInetAddress());
-                    HttpServerConnection conn = this.connFactory.createConnection(socket);
-
-                    // Start worker thread
-                    Thread t = new WorkerThread(this.httpService, conn);
-                    t.setDaemon(true);
-                    System.out.println("print #2");
-                  
-                    t.start();
-                    System.out.println("print #3");
-                } catch (InterruptedIOException ex) {
-                    break;
-                } catch (IOException e) {
-                    System.err.println("I/O error initialising connection thread: "
-                            + e.getMessage());
-                    break;
-                }
-            }
-        }
-    }
-
-    static class WorkerThread extends Thread {
-
-        private final HttpService httpservice;
-        private final HttpServerConnection conn;
-
-        public WorkerThread(
-                final HttpService httpservice,
-                final HttpServerConnection conn) {
-            super();
-            this.httpservice = httpservice;
-            this.conn = conn;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("New connection thread");
-            HttpContext context = new BasicHttpContext(null);
-            try {
-                while (!Thread.interrupted() && this.conn.isOpen()) {
-                    this.httpservice.handleRequest(this.conn, context);
-                }
-            } catch (ConnectionClosedException ex) {
-                System.err.println("Client closed connection");
-            } catch (IOException ex) {
-                System.err.println("I/O error: " + ex.getMessage());
-            } catch (HttpException ex) {
-                System.err.println("Unrecoverable HTTP protocol violation: " + ex.getMessage());
-            } finally {
-                try {
-                    this.conn.shutdown();
-                } catch (IOException ignore) {}
-            }
-        }
+        System.out.println("Finished all task executor threads");
 
     }
-}
     
+}
