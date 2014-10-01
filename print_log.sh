ef33d1767be1c27353523802dc0e8c86800f22bc
Anastasios Antoniadis <a.antoniadis1989@gmail.com>
2014-10-01 10:11:33 +0300

    done for presentation.

--- print_log.sh
+++ print_log.sh
@@ -1,2 +1,38 @@
 head -1 ./log/stats.txt #prints the 1st line of file
 tail -1 ./log/stats.txt #prints the last line of file
+echo
+head -1 ../SparrowWorker_1/log/stats.txt #prints the 1st line of file
+tail -1 ../SparrowWorker_1/log/stats.txt #prints the last line of file
+echo
+
+head -1 ../SparrowWorker_2/log/stats.txt #prints the 1st line of file
+tail -1 ../SparrowWorker_2/log/stats.txt #prints the last line of file
+echo
+
+head -1 ../SparrowWorker_3/log/stats.txt #prints the 1st line of file
+tail -1 ../SparrowWorker_3/log/stats.txt #prints the last line of file
+echo
+
+head -1 ../SparrowWorker_4/log/stats.txt #prints the 1st line of file
+tail -1 ../SparrowWorker_4/log/stats.txt #prints the last line of file
+echo
+
+head -1 ../SparrowWorker_5/log/stats.txt #prints the 1st line of file
+tail -1 ../SparrowWorker_5/log/stats.txt #prints the last line of file
+echo
+
+head -1 ../SparrowWorker_6/log/stats.txt #prints the 1st line of file
+tail -1 ../SparrowWorker_6/log/stats.txt #prints the last line of file
+echo
+
+head -1 ../SparrowWorker_7/log/stats.txt #prints the 1st line of file
+tail -1 ../SparrowWorker_7/log/stats.txt #prints the last line of file
+echo
+
+head -1 ../SparrowWorker_8/log/stats.txt #prints the 1st line of file
+tail -1 ../SparrowWorker_8/log/stats.txt #prints the last line of file
+echo
+
+head -1 ../SparrowWorker_9/log/stats.txt #prints the 1st line of file
+tail -1 ../SparrowWorker_9/log/stats.txt #prints the last line of file
+echo
